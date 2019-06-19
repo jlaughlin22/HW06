fef92559161e6194cfdbf281bf97e20da49967f0
jlaughlin22 <46356607+jlaughlin22@users.noreply.github.com>
2019-06-18 22:48:19 -0700

    comments

--- headers/Senior.h
+++ headers/Senior.h
@@ -4,11 +4,12 @@
 
 class Senior: public Customer{
     private:
-    const double SAVINGS_INTEREST;
-    const double CHECK_INTEREST;
-    const double CHECK_CHARGE;
-    const double OVERDRAFT_PENALTY;
+    const double SAVINGS_INTEREST;//interest on savings
+    const double CHECK_INTEREST;//interest on checking
+    const double CHECK_CHARGE;//charge for check
+    const double OVERDRAFT_PENALTY;//everdraft penalty
     public:
+    //constructor
     Senior(std::string name, std::string address, std::string telephone_number, int age, int customer_number): 
         Customer(name, address, telephone_number, age, customer_number, "senior"), 
         SAVINGS_INTEREST(8.5),
@@ -16,16 +17,16 @@ class Senior: public Customer{
         CHECK_CHARGE(27.00),
         OVERDRAFT_PENALTY(30.00) {}
 
-    double get_check_charge(){
+    double get_check_charge(){//accessor for check_charge
         return CHECK_CHARGE;
     }
-    double get_overdraft(){
+    double get_overdraft(){//accessor for overdraft
         return OVERDRAFT_PENALTY;
     }
-    double get_savings_interest(){
+    double get_savings_interest(){//accessor for savings_interest
         return SAVINGS_INTEREST;
     }
-    double get_check_interest(){
+    double get_check_interest(){//accessor for check_interest
         return CHECK_INTEREST;
     }
 };
